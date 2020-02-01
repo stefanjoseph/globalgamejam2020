5a2742bda7e54487ac76e093727febac534975fb
stefanjoseph <stefanjc2@gmail.com>
2020-02-01 10:04:25 -0800

    Added initial tutorial files

---
+++ game.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+  var config = {
+    width: 1200,
+    height: 600,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2]
+  }
+
+
+
+  var game = new Phaser.Game(config);
+}
