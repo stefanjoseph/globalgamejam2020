8bfcecf67069486c52884c431cf770e7658bec90
Stefan Joseph <Stefan@Stefans-MacBook-Pro.local>
2020-02-01 13:37:17 -0800

    got some rotating circles

--- BuildScene.js
+++ BuildScene.js
@@ -6,18 +6,35 @@ class BuildScene extends Phaser.Scene {
   create(){
     this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
 
-    this.ball = this.add.image(400,400,"ball");
-    // this.ball.setOrigin(0,0);
+    this.layers = [];
+    this.num_layers = 3;
 
-    // this.circle = new Phaser.Geom.Circle(400, 300, 100);
+    var rot = 5;
+    var scale = 1.0;
+
+    for(var i = 0; i < this.num_layers; i++) {
+      this.layers.push( {image: this.add.image(400,400, "ball"), rotationSpeed: rot} );
+      this.layers[i].image.setScale(scale, scale);
+      rot *= -1;
+      scale *= 0.75;
+    }
+
+    // this.circle = new Phaser.Geom.Circle(400, 200, 50);
+
     // var graphics = this.add.graphics({ fillStyle: { color: 0xff0000 } });
     // graphics.fillCircleShape(this.circle);
+
   }
 
 
   update(){
+    // Phaser.Math.RotateAroundDistance(ball2, ball1.x, ball1.y, angle1, distance1);
 
-    this.ball.angle = this.ball.angle+0.5;
+    this.layers.forEach(function (layerPair, index) {
+      layerPair.image.angle = layerPair.image.angle + layerPair.rotationSpeed;
+    });
+
+    // console.log(this.ball.rotation);
 
   }
 
---
+++ RotatingLayer.js
@@ -0,0 +1,8 @@
+class RotatingLayer {
+  constructor(imageName, rot, scene){
+    //Rotation Speed/Direction
+    this.rotation = rot
+    //Circular body
+    return {scene.add.image(400,400,imageName, rot)};
+  }
+}
